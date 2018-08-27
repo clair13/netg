17381200bf3333a13b396a7815ba0c5cbeea03e3
clair13 <theclairvoyant1313@yahoo.gr>
2018-08-27 14:38:22 +0300

    Implement new and create action and corresponding view

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -2,4 +2,24 @@ class CitationsController < ApplicationController
   def index
     @citations = Citation.all
   end
+
+  def new
+    @citation = Citation.new
+  end
+
+  def create
+    @citation = Citation.new(citation_params)
+
+    if @citation.save
+      redirect_to citations_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def citation_params
+    params.require(:citation).permit(:title)
+  end
 end
